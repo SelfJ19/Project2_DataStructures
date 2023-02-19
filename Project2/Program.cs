1e652927d1d4d3fbb7f8ddc67754c193f6c6075a
SelfJ19 <SELFJ1@ETSU.EDU>
2023-02-19 15:54:52 -0500

    added XML comments to binary search

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Project2
 {
@@ -9,20 +10,34 @@ namespace Project2
             int[][] numbers = new int[20][];
                         
             StreamReader reader = new StreamReader(@"..\..\..\inputJagged.csv");
-            string line = reader.ReadLine();
-            while((line = reader.ReadLine()) != null)
+
+            // Reads lines while there are still lines to be read
+            while (!reader.EndOfStream)
             {
-                string[] num = line.Split(",");
-                Convert.ToInt32(num);
-                for (int i = 0; i < num.Length; i++)
+                // Loops through each of the 20 jagged arrays in numbers
+                for(int i = 0; i < numbers.Length; i++)
                 {
-                    
+                    var eachLine = reader.ReadLine();
+                    var numValues = eachLine.Split(',');
+                    numbers[i] = new int[numValues.Length];
+
+                    // Loops through each of the j values in the numbers[i] array
+                    for(int j = 0; j < numValues.Length; j++)
+                    {
+                        numbers[i][j] = Convert.ToInt32(numValues[j]);
+                    }
                 }
             }
-            Console.WriteLine(line);
         }
 
         #region QuickSort()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="leftIndex"></param>
+        /// <param name="rightIndex"></param>
+        /// <returns></returns>
         public int[] QuickSort(int[] numbers, int leftIndex, int rightIndex)
         {
             int a = leftIndex;
@@ -61,9 +76,40 @@ namespace Project2
         }
         #endregion
 
-        public void BinarySearch()
+        #region BinarySearch()
+        /// <summary>
+        /// Method that searches through the array until it either finds or doesn't find the value
+        /// </summary>
+        /// <param name="numbers">name of the array</param>
+        /// <param name="targetValue">the value being searched for</param>
+        /// <returns>either the value that was being searched for or -1 if not found</returns>
+        public int BinarySearch(int[] numbers, int targetValue)
         {
+            bool checker = false;
+            int counter = 0;
+            int firstIndex = 0;
+            while (counter < numbers.Length -1)
+            {
+                int middleIndex = ((firstIndex + (numbers.Length - 1)) / 2);
 
+                if(numbers[middleIndex] == targetValue)
+                {
+                    checker = true;
+                    return targetValue;
+                }
+                else if (numbers[middleIndex] > targetValue)
+                {
+                    firstIndex--;
+                    counter++;
+                }
+                else if(numbers[middleIndex] < targetValue)
+                {
+                    firstIndex++;
+                    counter++;
+                }
+            }
+            return -1;
         }
+        #endregion
     }
 }
